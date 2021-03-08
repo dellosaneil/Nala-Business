8d8fe163551eccab53daac471eb8f8d5611987f1
DellosaNeil <dellosaneil@gmail.com>
2021-03-09 07:24:44 +0800

    added dependencies

---
+++ app/src/main/java/com/example/nalasbusinesstracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nalasbusinesstracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
