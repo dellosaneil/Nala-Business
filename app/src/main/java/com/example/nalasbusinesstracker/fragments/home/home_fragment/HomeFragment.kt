8332e0fde697aafba1d9be3491f59d4661ce1ec2
DellosaNeil <dellosaneil@gmail.com>
2021-03-11 09:45:05 +0800

    dynamically put chips in chipgroup

--- app/src/main/java/com/example/nalasbusinesstracker/fragments/home/home_fragment/HomeFragment.kt
+++ app/src/main/java/com/example/nalasbusinesstracker/fragments/home/home_fragment/HomeFragment.kt
@@ -1,13 +1,14 @@
 package com.example.nalasbusinesstracker.fragments.home.home_fragment
 
 import android.content.Context
-import android.content.res.Configuration
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
@@ -15,11 +16,13 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.nalasbusinesstracker.R
 import com.example.nalasbusinesstracker.databinding.FragmentHomeBinding
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class HomeFragment : Fragment(), HomeAdapter.HomeClothingClicked{
+class HomeFragment : Fragment(), HomeAdapter.HomeClothingClicked {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
@@ -28,7 +31,7 @@ class HomeFragment : Fragment(), HomeAdapter.HomeClothingClicked{
     private val TAG = "HomeFragment"
 
 
-    private val scrollListener = object : RecyclerView.OnScrollListener(){
+    private val scrollListener = object : RecyclerView.OnScrollListener() {
         override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
             super.onScrolled(recyclerView, dx, dy)
             if (dy > 0) {
@@ -38,6 +41,36 @@ class HomeFragment : Fragment(), HomeAdapter.HomeClothingClicked{
             }
         }
     }
+
+    private fun addChips() {
+        val chipNames = arrayOf("Dress", "Shirt", "Polo", "Pants", "Chicken", "Woof")
+        repeat(chipNames.size) {
+            Chip(requireContext()).apply {
+                isCheckable = true
+                isChipIconVisible = false
+                isCheckedIconVisible = false
+                checkedIcon = getDrawable(requireContext(), R.drawable.ic_mtrl_chip_checked_black)
+                setChipBackgroundColorResource(R.color.mtrl_choice_chip_background_color)
+                setRippleColorResource(R.color.mtrl_choice_chip_ripple_color)
+                text = chipNames[it]
+                binding.homeFragmentChipGroup.addView(this)
+            }
+        }
+    }
+
+//    <item name="android:checkable">true</item>
+//
+//    <item name="chipIconVisible">false</item>
+//    <item name="checkedIconVisible">false</item>
+//    <item name="closeIconVisible">false</item>
+//
+//    <item name="checkedIcon">@drawable/ic_mtrl_chip_checked_black</item>
+//
+//    <item name="android:textColor">@color/mtrl_choice_chip_text_color</item>
+//
+//    <item name="chipBackgroundColor">@color/mtrl_choice_chip_background_color</item>
+//    <item name="rippleColor">@color/mtrl_choice_chip_ripple_color</item>
+
 
     override fun onResume() {
         super.onResume()
@@ -61,6 +94,7 @@ class HomeFragment : Fragment(), HomeAdapter.HomeClothingClicked{
         super.onViewCreated(view, savedInstanceState)
         initializeRecyclerView()
         floatingActionButton(view)
+        addChips()
     }
 
     private fun floatingActionButton(view: View) {
@@ -113,6 +147,4 @@ class HomeFragment : Fragment(), HomeAdapter.HomeClothingClicked{
         val screenWidthDp = displayMetrics.widthPixels / displayMetrics.density
         return (screenWidthDp / 162 + 0.5).toInt()
     }
-
-
 }
